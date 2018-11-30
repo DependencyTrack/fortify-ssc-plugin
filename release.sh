3228b421c1478f5b7664d301aa5f7fd99d3af836
Steve Springett <steve@springett.us>
2018-11-30 12:28:38 -0600

    added github release plugin

--- release.sh
+++ release.sh
@@ -2,13 +2,14 @@
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 export PATH=JAVA_HOME/bin:$PATH
 
-read -p "Really deploy to Maven Central repository (Y/N)? "
+read -p "Really deploy to Maven Central repository and GitHub releases (Y/N)? "
 if ( [ "$REPLY" == "Y" ] ) then
 
   mvn clean
   mvn release:clean
   mvn release:prepare -DupdateWorkingCopyVersions=false
   mvn release:perform -Prelease -X -e | tee release.log
+  mvn github-release:release
 
 else
   echo -e "Exit without deploy"
